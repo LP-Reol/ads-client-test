a916a051e59fd5e783f39f8055bd743fb1c5cbf8
Jakob Bak <jakob.bak@pm.me>
2023-05-26 14:23:44 +0200

    added ads-client, and it works!

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 
 const express = require('express'); //Import the express dependency
+const ads = require('ads-client')
+
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 8000;                  //Save the port number where your server will be listening
+
+const client = new ads.Client({
+    targetAmsNetId: 'localhost',
+    targetAdsPort: 851,
+})
+
+client.connect()
+    .then(res => {
+        console.log('Connected to the ${res.targetAmsNetId}')
+        console.log('Router assigned us AmsNetId ${res.localAmsNetId} and port ${res.localAdsPort}')
+        return client.disconnect()
+    })
+    .then(() => {
+        console.log('Disconnected')
+    })
+    .catch(err => {
+        console.log('Something Failed:', err)
+    })
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
