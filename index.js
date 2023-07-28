0080b34baf0d73f3076ada32c6c78765444004a4
Christian Rishøj <christian@rishoj.net>
2023-07-28 17:00:35 +0200

    basic connection test

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-
 const express = require('express'); //Import the express dependency
 const ads = require('ads-client')
 
-const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 8000;                  //Save the port number where your server will be listening
+const app = express();     // Instantiate an express app, the main work horse of this server
+const port = 8000;              // Save the port number where your server will be listening
 
 const client = new ads.Client({
-    targetAmsNetId: 'localhost',
+    localAmsNetId: '192.168.0.20.1.1',  // Can be anything but needs to be in PLC StaticRoutes.xml file
+    localAdsPort: 32750,                // Can be anything that is not used
+    targetAmsNetId: '172.16.21.180.1.1',
     targetAdsPort: 851,
+    routerAddress: '192.168.0.30',
+    routerTcpPort: 48898
 })
 
 client.connect()
     .then(res => {
         console.log(`Connected to the ${res.targetAmsNetId}`)
         console.log(`Router assigned us AmsNetId ${res.localAmsNetId} and port ${res.localAdsPort}`)
-        return client.disconnect()
+        client.readSymbol('MAIN.cnt').then((res) => {
+            console.log(`cnt = ${res.value}`)
+            return client.disconnect()
+        })
     })
     .then(() => {
         console.log('Disconnected')
@@ -23,12 +29,6 @@ client.connect()
         console.log('Something Failed:', err)
     })
 
-//Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
-
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${port}`); 
+    console.log(`Now listening on port ${port}`);
 });
